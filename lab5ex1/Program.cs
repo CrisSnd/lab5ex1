ae4d7e9ef7b67ae9fe32fc63ecbc50f30e5918c7
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-18 18:50:23 +0300

    Add project files.

---
+++ lab5ex1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa pozitia unui substring intr-un string, ambele siruri de caractere fiind citite de la tastatura.*/
+
+
+            Console.WriteLine("Introduceti stringul: ");
+            string string1= (Console.ReadLine());
+
+
+           if (string.IsNullOrWhiteSpace(string1))
+            {
+                Console.WriteLine("Nu s-a introdus primul string.Introduceti stringul:");
+                return;
+            }
+
+
+
+            Console.WriteLine("Introduceti substringul:");
+             string substring= (Console.ReadLine());
+            if (string.IsNullOrWhiteSpace(substring))
+            {
+                Console.WriteLine("Nu s-a introdus substringul.Introduceti substringul: ");
+            }
+
+
+            int pozitieSubstring=string1.IndexOf(substring);
+
+            if (pozitieSubstring == -1)
+            {
+                Console.WriteLine("\n" + $"Substringul {substring} nu se afla in stringul {string1}");
+            }
+            else
+            { 
+                Console.WriteLine("\n"+$" Substringul {substring}  incepe de la pozitia {pozitieSubstring} in stringul {string1}. ");
+            }
+
+        }
+
+     }
+
+ }
+
+
+
+
+
+
+
+
